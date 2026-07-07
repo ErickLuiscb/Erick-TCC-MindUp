c744c0d4204631a37be208ce47e3dcece1d247aa
Erick Barneche <erickbarneche.pl020@academico.ifsul.edu.br>
2026-07-07 00:08:10 -0300

    Fix: Testando seeder

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -14,24 +14,24 @@ class UserSeeder extends Seeder
         User::firstOrCreate(
             ['email' => 'erick@gmail.com'],
             [
-                'nome'      => 'Erick',
+                'nome'      => 'Oliver',
                 'senha'     => Hash::make('123456'),
                 'tipo'      => 'psicologo',
-                'is_admin'  => true,
-                'role'      => 'admin',
-            ]
-        );
-
-        // Usuário comum
-        User::firstOrCreate(
-            ['email' => 'luis@gmail.com'],
-            [
-                'nome'      => 'Luis',
-                'senha'     => Hash::make('123456'),
-                'tipo'      => 'usuario',
                 'is_admin'  => false,
                 'role'      => 'user',
             ]
         );
+
+        // Usuário comum
+        // User::firstOrCreate(
+        //     ['email' => 'luis@gmail.com'],
+        //     [
+        //         'nome'      => 'Luis',
+        //         'senha'     => Hash::make('123456'),
+        //         'tipo'      => 'usuario',
+        //         'is_admin'  => false,
+        //         'role'      => 'user',
+        //     ]
+        // );
     }
 }
